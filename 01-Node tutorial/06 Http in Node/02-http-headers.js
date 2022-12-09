73bd5568a1067030fa4f6a47f34e7a04d7f0bfb2
Prachanda Rana <pralhadrana123@gmail.com>
2022-12-09 21:19:57 +0545

    node js

--- 01-Node tutorial/06 Http in Node/02-http-headers.js
+++ 01-Node tutorial/06 Http in Node/02-http-headers.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const server = http.createServer();
 
-// Content-type - html,json,plain
+// Content-type - text/html,text/json,text/plain , image/svg+xml
 
 server.on('request',(req,res)=>{
     res.writeHead(200,{'Content-type':'text/html'});
