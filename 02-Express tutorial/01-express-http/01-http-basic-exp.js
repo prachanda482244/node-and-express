68f580b04fb07c954e36e8666f0ecf5fef3fc376
Prachanda Rana <pralhadrana123@gmail.com>
2022-12-09 21:52:47 +0545

    express js

--- 02-Express tutorial/01-express-http/01-http-basic-exp.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const server = http.createServer()
-
-server.on('request',(req,res)=>{
-res.end('hello world');
-});
-
-server.listen(5000);
---
+++ 02-Express tutorial/01http-basic-exp.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+// Method of express server
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use
+// app.listen
+
+app.get('/',(req,res)=>{
+    // res.end('Hello world');
+    res.status(200).send('Hello world')
+});
+
+app.get('/about',(req,res)=>{
+    res.status(200).send('About page')
+})
+app.all('*',(req,res)=>{
+    res.status(404).send('404 page not found')
+})
+
+app.listen(5000,()=>{
+    console.log('Server is listening on 5000')
+})
