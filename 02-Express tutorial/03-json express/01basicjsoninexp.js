fae6fc6795b4537eb1c65885fc64acc0e492d27d
Prachanda Rana <pralhadrana123@gmail.com>
2022-12-21 18:54:20 +0545

    node and express

--- 02-Express tutorial/03-json express/01basicjsoninexp.js
+++ 02-Express tutorial/03-json express/01basicjsoninexp.js
@@ -3,16 +3,27 @@ const app = express();
 const{products,people} = require('../data');
 
 app.get('/',(req,res)=>{
-    res.status(200).send('Hello world');
+    res.status(200).send('<h1>Home page <a href="/bachhi">love</a> <a href="/api/products">Products</a></h1>')
 })
 
+app.get('/api/products',(req,res)=>{
+    const newProducts = products.map((product)=>{
+        const {id,name,price} = product
+        return {id, name, price}
+    })
+    res.json(newProducts)
+})
 app.get('/json',(req,res)=>{
     // res.json([{name:'itachi Uchiha'},{name:'izumi uchiha'}]);
     res.json(people)
 })
 
+app.get('/bachhi',(req,res)=>{
+    res.status(200).send('<h1>I love you maya ❤️</h1> <a href="/">Go back</a>')
+})
+
 app.use('*',(req,res)=>{
-    res.status(404).send('404 page not found');
+    res.status(404).send('<h1>404 page not found</h1>');
 })
 
 app.listen(5000,()=>{
