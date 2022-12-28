5dd38fdf2555ad2bf2ee87e0cc586d11a9077fdd
Prachanda Rana <62231260+prachanda482244@users.noreply.github.com>
2022-12-28 22:36:37 +0545

    post method

--- 02-Express tutorial/02Express-Application/02-all-static-exp.js
+++ 02-Express tutorial/02Express-Application/02-all-static-exp.js
@@ -4,13 +4,12 @@ const path = require('path');
 
 // Setting up static file and middleware
 app.use(express.static('./public'));
-
 // app.get('/',(req,res)=>{
 //     res.sendFile(path.resolve(__dirname,'./public/index.html'));
 // adding to public assets
 // SSR - SERVER SIDE RENDERING
 // })
 
-app.listen(5000,()=>{
+app.listen(5000, () => {
     console.log('Server running');
 })
--- 02-Express tutorial/05-http method in express/02-method-post.js
+++ 02-Express tutorial/05-http method in express/02-method-post.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('../method-public'));
+// app.use(express.static('../02Express-Application/public'));
+app.use(express.static('../method-public'))
+
+// parse form data
+app.use(express.urlencoded({ extended: false }))
+
+app.post('/login', (req, res) => {
+    const { name } = req.body;
+    if (name) {
+        return res.status(200).send(`Welcome ${name}`)
+    }
+    res.status(401).send('Please provide credentials')
+})
 
 app.listen(5000, () => {
-    console.log('Server running');
+    console.log('Server is running');
 })
